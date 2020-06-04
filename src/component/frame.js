97f8e47eca2ba87cbed2b21bc9fa1aaa5661bc5d
metthew9210 <metthew.92.10.01@gmail.com>
2020-06-04 16:36:44 +0900

    change title and change content height

--- src/component/frame.js
+++ src/component/frame.js
@@ -6,12 +6,12 @@ class frame extends Component
     render() {
         return (
             <div className="frame">
-              <div className="frame-header">스키마</div>
-              <div className="frame-header">테이블</div>
-              <div className="frame-header">컬럼</div>
-              <div className="frame-header">주석</div>
-              <div className="frame-body">본문</div>
-              <div className="frame-footer">하단</div>
+              <div className="frame-header"></div>
+              <div className="frame-header"></div>
+              <div className="frame-header"></div>
+              <div className="frame-header"></div>
+              <div className="frame-body"></div>
+              <div className="frame-footer"></div>
             </div>
         )
     }
